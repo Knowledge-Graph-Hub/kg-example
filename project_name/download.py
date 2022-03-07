6559fdaf2553246bd6c0808d33808a4ea7617144
caufieldjh <j.harry.caufield@gmail.com>
2022-03-07 15:58:25 -0500

    Add snippet option for downloader

--- project_name/download.py
+++ project_name/download.py
@@ -5,12 +5,13 @@
 from kghub_downloader.download_utils import download_from_yaml # type: ignore
 
 
-def download(yaml_file: str, output_dir: str, ignore_cache: bool = False) -> None:
+def download(yaml_file: str, output_dir: str, snippet_only: bool, ignore_cache: bool = False) -> None:
     """Downloads data files from list of URLs (default: download.yaml) into data directory (default: data/).
 
     Args:
         yaml_file: A string pointing to the yaml file utilized to facilitate the downloading of data.
         output_dir: A string pointing to the location to download data to.
+        snippet_only: Downloads only the first 5 kB of the source, for testing and file checks. 
         ignore_cache: Ignore cache and download files even if they exist [false]
 
     Returns:
@@ -18,6 +19,7 @@ def download(yaml_file: str, output_dir: str, ignore_cache: bool = False) -> Non
     """
 
     download_from_yaml(yaml_file=yaml_file, output_dir=output_dir,
-                       ignore_cache=ignore_cache)
+                        snippet_only=snippet_only,
+                        ignore_cache=ignore_cache)
 
     return None
